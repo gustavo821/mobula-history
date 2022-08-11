feba2fecb5c0c3a456f200ddd40f16c359ce4dd7
Sacha Marcus <89188530+NBMSacha@users.noreply.github.com>
2022-08-11 11:17:10 +0200

    core: Initial push

---
+++ MagicWeb3.ts
@@ -0,0 +1,137 @@
+import axios from "axios";
+import Web3 from "web3";
+import Web3HttpProvider from "web3-providers-http";
+import { HttpsProxyAgent } from "https-proxy-agent";
+import { AbiItem } from "web3-utils";
+export class MagicWeb3 {
+  rpcs: string[];
+  proxies: string[];
+  alert: boolean;
+  index: number;
+
+  constructor(rpcs: string[] | string, proxies: string[] = [], alert = true) {
+    this.rpcs = typeof rpcs == "string" ? [rpcs] : rpcs;
+    this.alert = alert;
+    this.proxies = proxies;
+    this.fetchProxies();
+    this.index = 0;
+    // setInterval(() => this.fetchProxies(), 60000);
+  }
+
+  eth() {
+    const proxy = this.proxies[this.index % this.proxies.length];
+    this.index++;
+    // console.log('Fetching with ', proxy)
+    return new Web3(
+      new (Web3HttpProvider as any)(
+        this.rpcs[Math.floor(Math.random() * this.rpcs.length)],
+        {
+          keepAlive: true,
+          timeout: 20000, // milliseconds,
+          withCredentials: false,
+          agent: {
+            https: new HttpsProxyAgent(proxy),
+          },
+        }
+      )
+    ).eth;
+  }
+
+  contract(abi: AbiItem | AbiItem[], address: string) {
+    const proxy = this.proxies[this.index % this.proxies.length];
+    this.index++;
+    const web3 = new Web3(
+      new (Web3HttpProvider as any)(
+        this.rpcs[Math.floor(Math.random() * this.rpcs.length)],
+        {
+          keepAlive: true,
+          timeout: 20000,
+          withCredentials: false,
+          agent: {
+            https: new HttpsProxyAgent(proxy),
+          },
+        }
+      )
+    );
+    return new web3.eth.Contract(abi, address);
+  }
+
+  private async fetchProxies() {
+    let data: string[] = [];
+    try {
+      for (let i = 1; i <= this.proxies.length / 250; i++) {
+        data = data.concat(
+          (
+            await axios.get(
+              "https://proxy.webshare.io/api/proxy/list/?page=" + i,
+              {
+                headers: {
+                  Authorization:
+                    "Token a9b1c399948e3ccebe42d363fec3c3ef5f00c7e4",
+                },
+              }
+            )
+          )?.data?.results?.map((proxy: any) => {
+            return (
+              "http://" +
+              proxy.username +
+              ":" +
+              proxy.password +
+              "@" +
+              proxy.proxy_address +
+              ":" +
+              proxy.ports.http
+            );
+          })
+        );
+      }
+
+      if (data && data.length > 0) {
+        console.log("(MagicWeb3) : updated proxies");
+        this.proxies = data;
+      }
+    } catch (e) {
+      console.log("Proxies are down. Impossible to refresh proxies.");
+      if (this.alert) {
+        axios
+          .post(
+            "https://hooks.slack.com/services/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",
+            {
+              channel: "high-severity",
+              text: "Proxies are down. Impossible to refresh proxies.",
+            }
+          )
+          .catch((e) => {
+            console.log("MagicWeb3) Slack error : " + e);
+          });
+      }
+    }
+  }
+}
+
+export const loadProxies = async (pages: number) => {
+  let data: string[] = [];
+  for (let i = 1; i <= pages; i++) {
+    data = data.concat(
+      (
+        await axios.get("https://proxy.webshare.io/api/proxy/list/?page=" + i, {
+          headers: {
+            Authorization: "Token a9b1c399948e3ccebe42d363fec3c3ef5f00c7e4",
+          },
+        })
+      )?.data?.results?.map((proxy: any) => {
+        return (
+          "http://" +
+          proxy.username +
+          ":" +
+          proxy.password +
+          "@" +
+          proxy.proxy_address +
+          ":" +
+          proxy.ports.http
+        );
+      })
+    );
+  }
+  return data;
+};
